6894bf30b2ef7c6f781be27cd8842e6d2e596e7f
Dominik Piatek <do.piatek@gmail.com>
2024-01-08 11:35:25 +0000

    Clear build errors

--- src/lib/ghost.ts
+++ src/lib/ghost.ts
@@ -1,6 +1,4 @@
-import fs from 'node:fs/promises';
+import blogData from "./dominik-piatek.ghost.2015-09-22.json";
 
-const url = new URL('./dominik-piatek.ghost.2015-09-22.json', import.meta.url);
-const blogData = await fs.readFile(url, 'utf-8');
-const posts = JSON.parse(blogData)["db"][0].data.posts;
+const posts = blogData["db"][0].data.posts;
 export { posts };
